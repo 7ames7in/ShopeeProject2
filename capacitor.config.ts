0e375b4fbd2ef7a3bc68cae50f5f1c1332726d23
7ames7in <7ames7in@gmail.com>
2026-06-15 20:32:28 +0900

    feat: add and configure @capacitor/keyboard plugin to fix iOS layout panning issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.shopee.ai.draftcreator',
   appName: 'Shopee AI Draft Creator',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Keyboard: {
+      resize: 'body',
+    },
+  },
 };
 
 export default config;
